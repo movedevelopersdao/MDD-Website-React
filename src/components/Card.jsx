0551d95e4f3b268592c42155733596ad4184311f
Shrey123ansh <shreyann56sh@gmail.com>
2023-06-21 13:33:13 +0530

    changes btn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,8 @@ const Card = () => {
                     <h1 className="card-writeUp-head">Build DApps on Aptos</h1>
                     <p   className="card-writeUp-desc">No more need to build alone, join our community to build alongside
                         thousands of other Move developers.</p>
-                    <button onclick="openAptosDocs()" className="card-btn">Start Building</button>
+                    <a href="https://aptos.dev/">
+                    <button onclick="" className="card-btn">Start Building</button></a>
                 </div>
 
             </div>
@@ -25,7 +26,8 @@ const Card = () => {
                     <h1 className="card-writeUp-head">Learn Move Language: Tutorials and Examples</h1>
                     <p className="card-writeUp-desc">Learn to code in Move language using our in browser step-by-step
                         lessons to take you from basics to building your own fully functional DApps on Aptos.</p>
-                    <button onclick="openInteractiveMoveCourse()"  className="card-btn">Learn</button>
+                        <a href="https://move-developers-dao.gitbook.io/aptos-move-by-example/">
+                    <button onclick="openInteractiveMoveCourse()"  className="card-btn">Learn</button></a>
                 </div>
             </div>
             <div className="card">
@@ -33,8 +35,8 @@ const Card = () => {
                     <img src="card3-removebg-preview.png" alt="" className="card-img"/>
                     <h1 className="card-writeUp-head">Grants and bounty on Aptos</h1>
                     <p className="card-writeUp-desc">We allow students, DAOs & foundations to host grant programs in the community. Projects are allowed to host bounties on Aptos.
-                    </p>
-                    <button className="special-btns card-btn">Explore</button>
+                    </p><a href="#">
+                    <button className="special-btns card-btn">Explore</button></a>
                 </div>
             </div>
             <div className="card">
@@ -42,8 +44,8 @@ const Card = () => {
                     <img src="card4-removebg-preview.png" alt="" className="card-img"/>
                     <h1 className="card-writeUp-head">Ease the Hiring Process</h1>
                     <p className="card-writeUp-desc">Based on a curated data, hire the perfect fit for your project using
-                        our extensive jobs board.</p>
-                    <button className="btn card-btn">Apply</button>
+                        our extensive jobs board.</p><a href="">
+                    <button className="btn card-btn">Apply</button></a>
                 </div>
             </div>
             <div className="card">
@@ -53,8 +55,8 @@ const Card = () => {
                     <p className="card-writeUp-desc">Connect with our team and get a chance to become the college
                         ambassador. Hold #MoveLang
                         workshops, hackathons in your college and explore the best in className opportunities in Web3.
-                    </p>
-                    <button onclick="mailTo()" className="btn card-btn">Connect</button>
+                    </p><a href="mailto:movedevelopersdao@gmail.com">
+                    <button onclick="mailTo()" className="btn card-btn">Connect</button></a>
                 </div>
             </div>
         </div>
